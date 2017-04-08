2af901aa3ecfded3a140fc76ec81cc42bd588d58
blackmiaool <blackmiaool@gmail.com>
2017-04-08 12:32:20 +0800

    fix login

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ class Cr {
     }
     login(nickname, password) {
         this.loginInfo = {
-            username,
+            username:nickname,
             password
         };
         return new Promise((resolve, reject) => {
